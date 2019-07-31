71fa9ee6c0cfd9dac293a7ff5f752f5cce993559
Peter Werner <pcw1995@yahoo.com>
2019-07-31 23:37:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,11 +10,6 @@ function theBeatlesPlay(musicians,instruments){
 }
 
 function johnLennonFacts(facts){
-  facts=[
-    'He was the last Beatle to learn to drive',
-    'He was never a vegetarian',
-    'He was a choir boy and boy scout',
-    'He hated the sound of his own voice'];
   let i = 0;
   while(i<facts.length){
     facts[i] = facts[i] + "!!!";
