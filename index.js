e6803859011d30c3a3853c824fb5b9345b0973b7
Peter Werner <pcw1995@yahoo.com>
2019-07-31 23:32:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function johnLennonFacts(facts){
     'He was the last Beatle to learn to drive',
     'He was never a vegetarian',
     'He was a choir boy and boy scout',
-    'He hated the sound of his own voice'];
+    'He hated the sound of his own voice','foo','bar'];
   let i = 0;
   while(i<facts.length){
     facts[i] = facts[i] + "!!!";
